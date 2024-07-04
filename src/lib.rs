2417df652401e272afc0b54c7f39ecf47dc497b2
terryllowery <tlowe33@my.wgu.edu>
2024-07-03 20:27:50 -0500

    updated health check name

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 /// Healthcheck handler
-async fn healthcheck() -> impl Responder {
+async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
 pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().route("/healthcheck", web::get().to(healthcheck)))
+    HttpServer::new(|| App::new().route("/healthcheck", web::get().to(health_check)))
         .bind("127.0.0.1:8000")?
         .run()
         .await
