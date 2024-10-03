38dc3364f5afe6111fe12b795360558ec78861d8
terryllowery <tlowe33@my.wgu.edu>
2024-10-03 08:00:50 -0500

    fixed mut on connection

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -31,7 +31,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
     let config = get_configuration().expect("Failed to read configuration.");
     let connection_string = config.database.connection_string();
-    let connection = PgConnection::connect(&connection_string)
+    let mut connection = PgConnection::connect(&connection_string)
     .await
     .expect("Failed to connect to Postgres.");
 
@@ -51,7 +51,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     assert_eq!(200, response.status().as_u16());
 
     let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
-    .fetch_one(&connection)
+    .fetch_one(&mut connection)
     .await
     .expect("Failed to fetch saved subscription.");
 
