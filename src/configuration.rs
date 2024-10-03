3906222ee724ea00dbc298ba6bde2823ef3abc57
terryllowery <tlowe33@my.wgu.edu>
2024-10-02 22:34:44 -0500

    added helper function for DB connection string

--- src/configuration.rs
+++ src/configuration.rs
@@ -16,6 +16,15 @@ pub struct DatabaseSettings {
     pub database_name: String,
 }
 
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+}
+
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let settings = config::Config::builder()
     .add_source(
@@ -25,3 +34,4 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
 
     settings.try_deserialize::<Settings>()
 }
+
