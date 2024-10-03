2dcc075e7d848e3f7a4d6296bdbdd98795691333
terryllowery <tlowe33@my.wgu.edu>
2024-10-02 22:08:56 -0500

    Added function to get config file

--- src/configuration.rs
+++ src/configuration.rs
@@ -14,3 +14,13 @@ pub struct DatabaseSettings {
     pub host: String,
     pub database_name: String,
 }
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let settings = config::Config::builder()
+    .add_source(
+        config::File::new("configuaration.yaml", config::FileFormat::Yaml)
+    )
+    .build()?;
+
+    settings.try_deserialize::<Settings>()
+}
