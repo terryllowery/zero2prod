365b81db3ed5dc7b52708b937f3a5b2fb037ad51
terryllowery <tlowe33@my.wgu.edu>
2024-10-03 22:58:59 -0500

    added bunyan logger

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
+use sqlx::PgPool;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
-use sqlx::PgPool;
-use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("Failed to set subscriber.");
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     // TODO: Remove this debug print
     println!("{:?}", configuration);
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = std::net::TcpListener::bind(address)?;
-    let connection = PgPool::connect(
-        &configuration.database.connection_string()
-    )
-    .await
-    .expect("Failed to connect to Postgres.");
+    let connection = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
     run(listener, connection)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,6 @@
 use actix_web::{web, HttpResponse};
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 use chrono::Utc;
 
@@ -11,13 +12,23 @@ pub struct FormData {
 /// Subscribe to the newsletter
 pub(crate) async fn subscribe(form: web::Form<FormData>, connection: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    log::info!(
-        "request_id: {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name
+    let request_span = tracing::info_span!(
+        "adding a new subscriber",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
     );
-    log::info!("request_id: {} - Adding a new subscriber.", request_id);
+    let _request_span_guard = request_span.enter();
+    // tracing::info!(
+    //     "request_id: {} - Adding '{}' '{}' as a new subscriber.",
+    //     request_id,
+    //     form.email,
+    //     form.name
+    // );
+    // tracing::info!("request_id: {} - Adding a new subscriber.", request_id);
+    let query_span = tracing::info_span!(
+        "Saving new subscriber to the database",
+    );
    match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id , email, name, created_at)
@@ -29,13 +40,14 @@ pub(crate) async fn subscribe(form: web::Form<FormData>, connection: web::Data<P
         Utc::now()
     )
     .execute(connection.as_ref())
+    .instrument(query_span)
     .await {
         Ok(_) => {
-            log::info!("request_id: {} - Subscriber added successfully.", request_id);
+            tracing::info!("request_id: {} - Subscriber added successfully.", request_id);
             HttpResponse::Ok().finish()
         },
         Err(e) => {
-            log::error!("request_id: {} - Failed to execute query: {:?}",request_id, e);
+            tracing::error!("request_id: {} - Failed to execute query: {:?}",request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
