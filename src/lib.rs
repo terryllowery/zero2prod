4893c2f6743300389e2c9ca0a1e89c70e83af3d4
Terry Lowery <Terry@Terrys-MacBook-Pro-14.local>
2025-09-12 21:37:07 -0500

    moved logic into libary

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub async fn run() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind("127.0.0.1:8080")?
+        .run().await
+}
